95c9b97ee5af9580df45718257f5979b9770ea5a
Joumana <joumana.el-dakar@etu.univ-grenoble-alpes.fr>
2021-07-02 11:26:02 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  title = 'FICHE DE RENSEIGNEMENTS DE STAGE - 2021/2022';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
